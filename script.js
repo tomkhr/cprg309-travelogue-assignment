2e3834a611fa78031e5a140da9730ccf5d06744c
Tom Khrustalev <53542370+tomkhr@users.noreply.github.com>
2025-01-17 23:05:35 -0700

    switched from automatic slider to slider with buttons

--- script.js
+++ script.js
@@ -4,6 +4,10 @@ let parisIndex = 0;
 let villeIndex = 0;
 let londonIndex = 0;
 
+const parisPrevButton = document.getElementById("parisPrevButton");
+const parisNextButton = document.getElementById("parisNextButton");
+
+
 const parisImages = ["assets/paris1.jpg", "assets/paris2.jpg", "assets/paris3.jpg"];
 // const villeImages = ["assets/ville1.jpg", "assets/ville2.jpg", "assets/ville3.jpg"];
 // const londonImages = ["assets/london1.jpg", "assets/london2.jpg", "assets/london3.jpg"];
@@ -14,12 +18,26 @@ function setImage() {
     parisSlider.src = parisImages[parisIndex];
 }
 
-function nextImage() {
+function parisNextImage() {
     if(parisIndex == 2){
-        parisIndex = 0
+        parisIndex = 0;
+        setImage();
+    }else{
+        parisIndex = (parisIndex + 1);
+        setImage();
     };
-    parisIndex = (parisIndex + 1);
-    setImage();
 }
 
-setInterval(nextImage, 2000);
+function parisPrevImage (){
+    if(parisIndex == 0){
+        parisIndex = 2;
+        setImage();
+    }else{
+        parisIndex = (parisIndex - 1);
+        setImage();
+    }
+}
+
+parisNextButton.addEventListener("click", parisNextImage);
+
+parisPrevButton.addEventListener("click", parisPrevImage);
