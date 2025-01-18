9410e8cf04a0a92c539ee67decc55dad9292bc8f
Tom Khrustalev <53542370+tomkhr@users.noreply.github.com>
2025-01-18 00:26:11 -0700

    duplicates slider to all three travel locations

--- script.js
+++ script.js
@@ -7,37 +7,121 @@ let londonIndex = 0;
 const parisPrevButton = document.getElementById("parisPrevButton");
 const parisNextButton = document.getElementById("parisNextButton");
 
+const villePrevButton = document.getElementById("villePrevButton");
+const villeNextButton = document.getElementById("villeNextButton");
+
+const londonPrevButton = document.getElementById("londonPrevButton");
+const londonNextButton = document.getElementById("londonNextButton");
+
 
 const parisImages = ["assets/paris1.jpg", "assets/paris2.jpg", "assets/paris3.jpg"];
-// const villeImages = ["assets/ville1.jpg", "assets/ville2.jpg", "assets/ville3.jpg"];
-// const londonImages = ["assets/london1.jpg", "assets/london2.jpg", "assets/london3.jpg"];
+const villeImages = ["assets/ville1.jpg", "assets/ville2.jpg", "assets/ville3.jpg"];
+const londonImages = ["assets/london1.jpg", "assets/london2.jpg", "assets/london3.jpg"];
+
+
+
+
+
+// image slider code for paris
 
 const parisSlider = document.getElementById("parisSlider");
 
-function setImage() {
+function setParisImage() {
     parisSlider.src = parisImages[parisIndex];
 }
 
 function parisNextImage() {
     if(parisIndex == 2){
         parisIndex = 0;
-        setImage();
+        setParisImage();
     }else{
         parisIndex = (parisIndex + 1);
-        setImage();
+        setParisImage();
     };
 }
 
 function parisPrevImage (){
     if(parisIndex == 0){
         parisIndex = 2;
-        setImage();
+        setParisImage();
     }else{
         parisIndex = (parisIndex - 1);
-        setImage();
+        setParisImage();
     }
 }
 
 parisNextButton.addEventListener("click", parisNextImage);
 
 parisPrevButton.addEventListener("click", parisPrevImage);
+
+
+
+
+
+// image slider for villefranche sur mer
+
+const villeSlider = document.getElementById("villeSlider");
+
+function setVilleImage() {
+    villeSlider.src = villeImages[villeIndex];
+}
+
+function villeNextImage() {
+    if(villeIndex == 2){
+        villeIndex = 0;
+        setVilleImage();
+    }else{
+        villeIndex = (villeIndex + 1);
+        setVilleImage();
+    };
+}
+
+function villePrevImage (){
+    if(villeIndex == 0){
+        villeIndex = 2;
+        setVilleImage();
+    }else{
+        villeIndex = (villeIndex - 1);
+        setVilleImage();
+    }
+}
+
+villeNextButton.addEventListener("click", villeNextImage);
+
+villePrevButton.addEventListener("click", villePrevImage);
+
+
+
+
+
+// image slider for london
+
+const londonSlider = document.getElementById("londonSlider");
+
+function setLondonImage() {
+    londonSlider.src = londonImages[londonIndex];
+}
+
+function londonNextImage() {
+    if(londonIndex == 2){
+        londonIndex = 0;
+        setLondonImage();
+    }else{
+        londonIndex = (londonIndex + 1);
+        setLondonImage();
+    };
+}
+
+function londonPrevImage (){
+    if(londonIndex == 0){
+        londonIndex = 2;
+        setLondonImage();
+    }else{
+        londonIndex = (londonIndex - 1);
+        setLondonImage();
+    }
+}
+
+londonNextButton.addEventListener("click", londonNextImage);
+
+londonPrevButton.addEventListener("click", londonPrevImage);
